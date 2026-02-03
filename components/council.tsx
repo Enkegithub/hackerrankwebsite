789cfd0e37cd7432170e5e71deff41e0cb5276f7
Enkegithub <niteshkushwaha484@gmail.com>
2026-02-04 01:01:32 +0530

    final pgoto

--- components/council.tsx
+++ components/council.tsx
@@ -11,8 +11,8 @@ export default function Council() {
     { id: 7, name: " Jangam Ashwik", role: "Social Media Head ", image: "/aswik.jpeg" },
     { id: 8, name: "  Chandel Sathish Kumar ", role: "Creative head", image: "/satis.jpeg" },
     { id: 9, name: " Bishal Bista", role: " Student Repesentative(BOYS)", image: "/bishal.jpeg" },
-    { id: 10, name: "Yepuri Neha", role: " Student Repesentative(GIRLS)", image: "/nehaa.jpeg" },
-    { id: 11, name: "  Vishwasree", role: "Student Repesentative(GIRLS)", image: "/viswa.png" },
+    { id: 10, name: "Yepuri Neha", role: " Student Repesentative(GIRLS)", image: "/nehaa.jpg" },
+    { id: 11, name: "  Vishwasree", role: "Student Repesentative(GIRLS)", image: "/viswa.jpg" },
     { id: 12, name: " Praveen Reddy", role: "Student Repesentative(BOYS)", image: "/pravin.jpeg" },
     
   ]
