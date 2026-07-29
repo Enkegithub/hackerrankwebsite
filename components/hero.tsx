94bcd8238eb84eb0289038d910d656ace97f02e3
Nitesh Kushwaha <154442081+Enkegithub@users.noreply.github.com>
2026-07-29 20:45:19 +0530

    Update event title from 'CREATE X' to 'TECH GENSIS'

--- components/hero.tsx
+++ components/hero.tsx
@@ -50,7 +50,7 @@ export default function Hero() {
               PRESENTS
             </span>
             <span className="block text-3xl sm:text-4xl md:text-5xl mt-2 text-primary">
-              “CREATE X”
+              “TECH GENSIS”
             </span>
           </h1>
 
@@ -98,7 +98,7 @@ export default function Hero() {
               </defs>
               <text fill="white" fontSize="13" letterSpacing="4">
                 <textPath href="#textCircle">
-                  • HACKER RANK • CREATE X  • AI • INNOVATION •
+                  • HACKER RANK • TECH GENSIS  • AI • INNOVATION •
                 </textPath>
               </text>
             </svg>
@@ -117,9 +117,9 @@ export default function Hero() {
               letterSpacing: "2px",
             }}
           >
-            CREATE
+            TECH
             <br />
-            X
+             GENSIS
           </div>
         </div>
       </div>
